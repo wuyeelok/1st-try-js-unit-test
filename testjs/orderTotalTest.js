3340b9331229760ba2988c6e1a4fd153e486f1bf
wuyeelok <wuyeelok@gmail.com>
2018-08-11 20:39:31 +0800

    Revert "Pass the test if there is only 1 test case"
    This reverts commit 3a7a1f917e1e12fc02e6940c1a09ac0da20f0e0e.

--- testjs/orderTotalTest.js
+++ testjs/orderTotalTest.js
@@ -1,19 +1,8 @@
-// Test Case #1
 if(orderTotal({
     items: [
         createItem('Dragon food', 8),
         createItem('Dragon cage (small)', 800)
     ]
 }) !== 808) {
-    throw new Error('Check fail: Happy path #1')
-}
-
-// Test Case #2
-if(orderTotal({
-    items: [
-        createItem('Dragon collar', 20),
-        createItem('Dragon chew toy', 40)
-    ]
-}) !== 60) {
-    throw new Error('Check fail: Happy path #2')
+    throw new Error('Check fail: Happy path')
 }
