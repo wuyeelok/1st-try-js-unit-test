9d872f234c9207be6345e812145310c77924a563
wuyeelok <wuyeelok@gmail.com>
2018-08-12 20:54:02 +0800

    Add more test case

--- testjs/appTest.js
+++ testjs/appTest.js
@@ -3,6 +3,14 @@
 //  Parse the data
 //  Print the data
 
+
+// Test Case: 103 connect success
+if(connectTreeHouseUserAPI('https://teamtreehouse.com/chalkers.json'
+) == undefined || connectTreeHouseUserAPI('https://teamtreehouse.com/chalkers.json'
+) == null) {
+    throw new Error('Fail Test Case: 103 connect success')
+}
+
 // Test Case: 101 print chalkders info
 if(printUsersInfo(['chalkers'
 ]) !== 'chalkers has 196 total bage(s) and 5975 points in JavaScript') {
@@ -12,5 +20,5 @@ if(printUsersInfo(['chalkers'
 // Test Case: 102 print davemcfarland info
 if(printUsersInfo(['davemcfarland'
 ]) !== 'chalkers has 227 total bage(s) and  8241 points in JavaScript') {
-    throw new Error('Test Case: 102 print davemcfarland info')
+    throw new Error('Fail Test Case: 102 print davemcfarland info')
 }
