10f793903a2a5be52fdbc711b5c44fcc002049d0
kennethwu <kennethwu@leadstec.com>
2018-08-13 17:47:02 +0800

    Add Colors

--- js/app.js
+++ js/app.js
@@ -58,11 +58,12 @@ const getProfile = usersNameArray => {
 
 let unsecurePlainTextPassword = "password"
 
+let colors = require('colors')
 let bcrypt = require('bcrypt')
 bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(unsecurePlainTextPassword, salt, function(err, hash) {
         // Store hash in your password DB.
-        console.log(hash)
+        console.log(hash.green)
     });
 });
 
