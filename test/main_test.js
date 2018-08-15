40e0544aa2f2d4734547319f3f383a61cee38be2
kennethwu <kennethwu@leadstec.com>
2018-08-15 14:55:27 +0800

    First mocha test

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect
+
+// Test Suite
+describe('Mocha', () => {
+    // Test spc (unit test)
+    it('should run our tests using npm', () => {
+        expect(true).to.be.ok
+    })
+})
