8b07a0fe05ae213e124058e02e446d165ec2e39e
kennethwu <kennethwu@leadstec.com>
2018-08-15 12:35:03 +0800

    Breaking difficult test case into smaller

--- js/textUiltiles.js
+++ js/textUiltiles.js
@@ -2,6 +2,12 @@ const expect = require('chai').expect;
 
 // expect(true).to.be.true;
 
-const titleCase = str => str;
+const titleCase = str => {
+    return str[0].toUpperCase() + str.substring(1);
+}
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('t')).to.equal('T');
+expect(titleCase('coke')).to.equal('Coke');
+
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
