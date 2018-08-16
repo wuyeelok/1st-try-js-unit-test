f5124078280995f50021aa1fd13425dc1fa2c5f4
kennethwu <kennethwu@leadstec.com>
2018-08-16 16:06:17 +0800

    Mocks and Stubs

--- test/game_test.js
+++ test/game_test.js
@@ -1,9 +1,21 @@
-const expect = require('chai').expect
+var expect = require('chai').expect;
 
-describe('GAME INSTANCE FUNCTIONS', () => {
-
-    describe('checkGameStatus', () => {
-
-        it('should tell me when the game is over');
-    })
-})
+describe('GAME INSTANCE FUNCTIONS', function () {
+  describe('checkGameStatus', function () {
+    var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+    it('should tell me when the game is over', function () {
+      var players = [
+        {
+					ships: [
+						{
+							locations: [[0, 0]],
+							damage: [[0, 0]]
+						}
+					]
+        }
+      ];
+      var actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+    });
+  });
+});
