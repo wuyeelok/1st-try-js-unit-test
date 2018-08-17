17af22f4ef54894785fed2925f1375350a7f53d7
kennethwu <kennethwu@leadstec.com>
2018-08-17 17:29:04 +0800

    Start to write the actual code in sandbox.js

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,11 +3,25 @@
 // using xxxlok as pw
 
 const fetch = require('node-fetch')
+const orderTotal = require('./order-total').orderTotal
+const createItem = require('./order-total').createItem
+
+/*
+const result = orderTotal(fetch, {
+    country: 'DE',
+        items: [
+            createItem('Dragon waffles', 20, 2)
+        ]
+})
+*/
+
+/*
 const result = fetch('https://vatapi.com/v1/country-code-check?code=DE', {
     headers: {
         'apikey': 'a52c1771e4f220f9005b15d8c45a5f1e'
     }    
 }).then(resp => resp.json())
+*/
 .then(data => console.log(data.rates.standard.value))
 
 
