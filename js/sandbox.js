97a87ea7826eb458e5401b59583194de65337318
wuyeelok <wuyeelok@gmail.com>
2018-08-18 11:54:47 +0800

    Remove no use comment

--- js/sandbox.js
+++ js/sandbox.js
@@ -13,18 +13,4 @@ const result = orderTotal(fetch, 'a52c1771e4f220f9005b15d8c45a5f1e', {
             createItem('Dragon waffles', 20, 2)
         ]
 }).then(result => console.log(result))
-
-
-
-/*
-const result = fetch('https://vatapi.com/v1/country-code-check?code=DE', {
-    headers: {
-        'apikey': 'a52c1771e4f220f9005b15d8c45a5f1e'
-    }    
-}).then(resp => resp.json())
-
-.then(data => console.log(data.rates.standard.value))
-*/
-
-
     
