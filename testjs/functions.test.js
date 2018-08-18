f47daa31c754ceda961e5d107bd2c34d155acfe1
wuyeelok <wuyeelok@gmail.com>
2018-08-18 21:48:55 +0800

    Using axios

--- testjs/functions.test.js
+++ testjs/functions.test.js
@@ -40,4 +40,12 @@ describe('functions object method:', () => {
         usernames = ['john', 'karen', 'admin']
         expect(usernames).toContain('admin')
     })
+
+    test('User fetched name should be Leanne Graham', () => {
+        expect.assertions(1)
+        return functions.fetchUser()
+        .then(data => {
+            expect(data.name).toEqual('Leanne Graham')
+        })
+    })
 })
